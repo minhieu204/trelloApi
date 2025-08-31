be9c3f6ea759996779d0c1697059d65acbe3c60e
minhieu204 <minhhieu13062004@gmail.com>
2025-08-31 14:37:56 +0700

    init base

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
 import express from 'express'
 
 const app = express()
 
 const hostname = 'localhost'
-
-const port = 8080
+const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(`hello Nguyen Minh Hieu, I'am running server at http://${hostname}:${port}/`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
 })
